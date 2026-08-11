a89d1f3497c4f63a2bd870168c7ccb8c7afa048c
李宇扬 <1247079250@qq.com>
2026-08-11 14:38:43 +0800

    fix: 移除自定义 Layout 包装，使用 VitePress 默认布局
    - 将 Layout 恢复为 DefaultTheme.Layout 默认值
    - 移除 ReadingProgress + StatusBar 的自定义 Layout 注入
    - 暂时移除自定义 MyLayout.vue 包装
    - 此改动用于排查客户端路由跳转失效问题

---
+++ .vitepress/theme/components/MyLayout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+import ReadingProgress from './ReadingProgress.vue'
+import StatusBar from './StatusBar.vue'
+</script>
+
+<template>
+  <DefaultTheme.Layout>
+    <template #layout-top>
+      <ReadingProgress />
+    </template>
+    <template #layout-bottom>
+      <StatusBar />
+    </template>
+  </DefaultTheme.Layout>
+</template>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,19 +1,14 @@
-import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 
-import StatusBar from './components/StatusBar.vue'
 import TerminalHome from './components/TerminalHome.vue'
 import PostsList from './components/PostsList.vue'
-import ReadingProgress from './components/ReadingProgress.vue'
 
 import './style/vars.css'
 import './style/nerd.css'
 
 export default {
   extends: DefaultTheme,
-  Layout: () => h(DefaultTheme.Layout, null, { 'layout-top': () => h(ReadingProgress),
-    'layout-bottom': () => h(StatusBar) }),
   enhanceApp({ app }) {
     app.component('TerminalHome', TerminalHome)
     app.component('PostsList', PostsList)
